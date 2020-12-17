9dfdd319f1309c22947f10cd24f5280fd5c210de
Matheus Michalski Puel <matheuspuel@gmail.com>
2020-12-17 12:00:02 -0300

    added v1 to images url

--- src/resources/Constansts.js
+++ src/resources/Constansts.js
@@ -1,1 +1,1 @@
-export const ASSETS_ROOT_URL = 'https://res.cloudinary.com/mmpcloud/image/upload/portfolio_geisa'
+export const ASSETS_ROOT_URL = 'https://res.cloudinary.com/mmpcloud/image/upload/v1/portfolio_geisa'
